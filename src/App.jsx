608e0c4b9814577b69b0a5c3acd7e2d924594a61
Renuka <renukaullegaddi03@gmail.com>
2026-06-01 18:15:50 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+function App(){
+  return(
+  <>
+  <div>
+    <img src="my-react-app\public\logo512.png" width="40px" alt="logo"></img>
+    <h1>Fun Facts about React</h1>
+    <ul>
+      <li>It is a library, not a framework</li>
+      <li>Uses a Virtual DOM for performance</li>
+      <li>Relies on component-driven architecture</li>
+      <li>Features JSX syntax</li>
+      <li>Enforces unidirectional data flow</li>
+    </ul>
+    </div>
+
+  <div>
+    <ol>
+      <li>Excited to learn react!</li>
+      <li>It has more job opportunities</li>
+      <li>Code re-usability</li>
+      <li>Readability</li>
+    </ol>
+  </div>
+  </>
+  )
+}
+
+export default App;
